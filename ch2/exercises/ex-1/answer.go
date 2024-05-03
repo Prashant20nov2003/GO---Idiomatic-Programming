e5b7dbc4ebc3b6fc2f639a69c5ed042865e67333
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2024-05-04 02:26:35 +0530

    Updated the Ch1 and Ch2

--- ch2/exercises/ex-1/answer.go
+++ ch2/exercises/ex-1/answer.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-func main() {
-	var i = 20
+func main(){
+	var i = 20;
 	var f = float64(i)
 	fmt.Println(i)
 	fmt.Println(f)
---
+++ ch2/exercises/ex-2/answer.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main (){
+	const value = 10
+	f := float64(value)
+	var i int  = value
+	fmt.Println(i ,f)
+}
